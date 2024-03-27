73bc7f0417ab8211aa620ecb79aa85e48d3b500c
Shufan Shahi <shufanshahi@gmail.com>
2024-03-27 20:55:59 +0600

    email service added

--- routes/files.js
+++ routes/files.js
@@ -51,4 +51,42 @@ router.post('/',(req, res)=>{
     // Response -> link
 });
 
+router.post('/send', async (req,res)=>{
+    
+    const {uuid, emailTo, emailFrom} = req.body;
+    //validate request
+    if(!uuid || !emailTo || !emailFrom)
+    {
+        return res.status(422).send({error: "All feilds are required"});
+    }
+
+    //Get Data from database
+    const file = await File.findOne({uuid: uuid});
+    if(file.sender){
+        return res.status(422).send({error:"Email already sent."});
+    }
+
+    file.sender = emailFrom;
+    file.recevier = emailTo;
+    const response = await file.save();
+
+    //send email
+
+    const sendMail = require('../services/emailService');
+    sendMail({
+        from: emailFrom,
+        to: emailTo,
+        subject: 'inshare file sharing',
+        text: `${emailFrom} shared a file with you`,
+        html: require('../services/emailTemplate')({
+            emailFrom: emailFrom,
+            downloadLink: `${process.env.APP_BASE_URL}/files/${file.uuid}`,
+            size: parseInt(file.size/1000)+'KB',
+            expires:'24 hours'
+        })
+    });
+    return res.send({sucess: true});
+
+});
+
 module.exports = router;
