822fe3e1c24c29b8f5c89e2c970f14e48f789f22
Shufan Shahi <shufanshahi@gmail.com>
2024-03-31 15:08:45 +0600

    added cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 const path = require('path');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
@@ -11,6 +12,13 @@ const connectDB = require('./config/db');
 
 connectDB();
 
+//cors
+const corsOption = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+    // ['https://localhost:3000', 'https://localhost:5000', 'https://localhost:3300']
+}
+
+app.use(cors(corsOption));
 //Template engine
 app.set('views',path.join(__dirname,'/views'));
 app.set('view engine', 'ejs');
